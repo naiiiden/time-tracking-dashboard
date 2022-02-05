7b89e135c43297b96db0b020d09f6e6a5b891f23
naiden <na4ko359@gmail.com>
2022-02-05 19:09:39 +0200

    added js functionality when toggling buttons

--- script/script.js
+++ script/script.js
@@ -7,22 +7,33 @@ fetch("script/data.json")
     let weeklyButton = document.getElementById("weeklyButton");
     let monthlyButton = document.getElementById("monthlyButton")
 
+    let work = document.getElementById("workTime");
+    let workLast = document.getElementById("workTimeLast")
+
     dailyButton.addEventListener("click", () => {
       dailyButton.classList.add("active");
       weeklyButton.classList.remove("active");
       monthlyButton.classList.remove("active");
-    })
+
+      work.textContent = data[0].timeframes.daily.current + "hrs";
+      workLast.textContent = `Last week - ${data[0].timeframes.daily.previous}hrs`
+    });
 
     weeklyButton.addEventListener("click", () => {
       weeklyButton.classList.add("active");
       monthlyButton.classList.remove("active");
       dailyButton.classList.remove("active");
+
+      work.textContent = data[0].timeframes.weekly.current + "hrs";
+      workLast.textContent = `Last week - ${data[0].timeframes.weekly.previous}hrs`
     });
 
     monthlyButton.addEventListener("click", () => {
       monthlyButton.classList.add("active");
       weeklyButton.classList.remove("active");
       dailyButton.classList.remove("active");
-    })
-    // document.getElementById("hui").textContent = data[0].timeframes.daily.current;
+
+      work.textContent = data[0].timeframes.monthly.current + "hrs";
+      workLast.textContent = `Last week - ${data[0].timeframes.monthly.previous}hrs`
+    });
   });
